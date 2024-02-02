9ee345444ee42bfdbedf3098badb26882fe41398
Ruxandra Stanciu <ruxi749@gmail.com>
2024-02-02 15:58:36 +0000

    added TabButton component and used incorporated props.children prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
   return (
@@ -20,6 +21,12 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Elements</h2>
+          <menu>
+            <TabButton>Component</TabButton>
+          </menu>
         </section>
       </main>
     </div>
